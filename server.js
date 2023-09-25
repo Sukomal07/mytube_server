4025847a9718c56cee6cee013259631c55a17ade
Sukomal07 <hypersukomal@gmail.com>
2023-09-25 12:13:47 +0530

    try again

--- server.js
+++ server.js
@@ -15,18 +15,10 @@ dotenv.config()
 app.use(cookieParser())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
-app.use((req, res, next) => {
-    res.header('Access-Control-Allow-Origin', process.env.FRONTEND_URL);
-    res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-    res.header('Access-Control-Allow-Credentials', true);
-    next();
-});
+
 app.use(cors({
     origin: process.env.FRONTEND_URL,
     credentials: true,
-    allowedHeaders: ["Content-Type", "Authorization"],
-    exposedHeaders: ["Content-Disposition"],
 }))
 
 app.use("/api/auth", authRoutes)
