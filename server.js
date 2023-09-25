a1f252afcbc329eb57154f041dd3dc0e575240c5
Sukomal07 <hypersukomal@gmail.com>
2023-09-25 09:43:49 +0530

    solve errors

--- server.js
+++ server.js
@@ -1,59 +1,46 @@
 import express from 'express'
 import dotenv from 'dotenv'
 import cookieParser from 'cookie-parser'
-import {connectDb} from './data/database.js'
+import { connectDb } from './data/database.js'
 import cors from 'cors'
 
 import authRoutes from './routes/auth.js'
-import userRoutes from'./routes/users.js'
+import userRoutes from './routes/users.js'
 import videoRoutes from './routes/videos.js'
 import commentRoutes from './routes/comment.js'
 
 const app = express()
 dotenv.config()
 
-
-//middlewars
-app.use((req, res, next) => {
-    res.setHeader('Access-Control-Allow-Origin', 'https://mytube-sukomal07.vercel.app');
-    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-    res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
-    res.setHeader('Access-Control-Allow-Credentials', true);
-    next();
-});
 app.use(cookieParser())
 app.use(express.json())
-app.use("*",
-    cors({
-        origin:[process.env.FRONTTEND_URL],
-        methods:["GET","POST","PUT","DELETE"],
-        credentials:true,
-        allowedHeaders: ["Content-Type", "Authorization"],
-        exposedHeaders: ["Content-Disposition"],
-    })
-)
+app.use(express.urlencoded({ extended: true }))
+app.use(cors({
+    origin: process.env.FRONTEND_URL,
+    credentials: true
+}))
 
 app.use("/api/auth", authRoutes)
 app.use("/api/users", userRoutes)
 app.use("/api/videos", videoRoutes)
 app.use("/api/comments", commentRoutes)
 
-app.use((err, req, res , next) =>{
+app.use((err, req, res, next) => {
     const status = err.status || 500
     const message = err.message || "Something went wrong , Please check again"
     return res.status(status).json({
-        success:false,
+        success: false,
         status,
         message
     })
 })
 
-app.get("/", (req, res) =>{
+app.get("/", (req, res) => {
     res.send("<h1>WORKING</h1>")
 })
 
 //running app
-app.listen(process.env.PORT , ()=>{
+app.listen(process.env.PORT, () => {
     connectDb()
     console.log(`server is working on port: ${process.env.PORT}`)
 })
